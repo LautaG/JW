e433e026616cc589487348c6e0a020562e001f62
LautaG <74028143+LautaG@users.noreply.github.com>
2020-11-05 23:02:42 -0300

    Add files via upload

---
+++ js/video_play.js
@@ -0,0 +1,73 @@
+         window.reproducir_1 = function () {
+            document.getElementById("video_1_cont_video").play();
+        };
+
+        function play_video_1() {
+        document.getElementById("vid_1").style.display = "block";
+            document.getElementById("video_1_cont_video").style.display = "block";
+            document.getElementById("video_1_cont_video").style.objectFit = "cover";
+            document.getElementById("video_1_cont_play").style.display = "none";
+        };
+
+           window.reproducir_2 = function () {
+            document.getElementById("video_2_cont_video").play();
+        };
+
+        function play_video_2() {
+            document.getElementById("vid_2").style.display = "block";
+            document.getElementById("video_2_cont_video").style.display = "block";
+            document.getElementById("video_2_cont_video").style.objectFit = "cover";
+            document.getElementById("video_2_cont_play").style.display = "none";
+        };
+
+           window.reproducir_drink_1 = function () {
+            document.getElementById("drink_slider_1_video").play();
+        };
+
+        function play_video_drink_1() {
+            document.getElementById("vid_drink_1").style.display = "block";
+            document.getElementById("drink_slider_1_video").style.display = "block";
+            document.getElementById("drink_slider_1_video").style.objectFit = "cover";
+            document.getElementById("drink_slider_1_play").style.display = "none";
+        };
+
+              window.reproducir_drink_2 = function () {
+            document.getElementById("drink_slider_2_video").play();
+        };
+
+        function play_video_drink_2() {
+            document.getElementById("vid_drink_2").style.display = "block";
+            document.getElementById("drink_slider_2_video").style.display = "block";
+            document.getElementById("drink_slider_2_video").style.objectFit = "cover";
+            document.getElementById("drink_slider_2_play").style.display = "none";
+        };
+              window.reproducir_drink_3 = function () {
+            document.getElementById("drink_slider_3_video").play();
+        };
+
+        function play_video_drink_3() {
+            document.getElementById("vid_drink_3").style.display = "block";
+            document.getElementById("drink_slider_3_video").style.display = "block";
+            document.getElementById("drink_slider_3_video").style.objectFit = "cover";
+            document.getElementById("drink_slider_3_play").style.display = "none";
+        };
+              window.reproducir_drink_4 = function () {
+            document.getElementById("drink_slider_4_video").play();
+        };
+
+        function play_video_drink_4() {
+            document.getElementById("vid_drink_4").style.display = "block";
+            document.getElementById("drink_slider_4_video").style.display = "block";
+            document.getElementById("drink_slider_4_video").style.objectFit = "cover";
+            document.getElementById("drink_slider_4_play").style.display = "none";
+        };
+              window.reproducir_drink_5 = function () {
+            document.getElementById("drink_slider_5_video").play();
+        };
+
+        function play_video_drink_5() {
+            document.getElementById("vid_drink_5").style.display = "block";
+            document.getElementById("drink_slider_5_video").style.display = "block";
+            document.getElementById("drink_slider_5_video").style.objectFit = "cover";
+            document.getElementById("drink_slider_5_play").style.display = "none";
+        };
